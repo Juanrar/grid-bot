522c2e08829bc92fe9202804a303ab85dda22c25
juanrar <juanchilorenzo@gmail.com>
2026-05-23 23:36:18 -0300

    Add initial project files including .gitignore, package.json, package-lock.json, and Binance client implementation

---
+++ src/binanceClient.js
@@ -0,0 +1,27 @@
+import axios from "axios"
+import crypto from "cripto"
+
+const BASE_URL = "https://api.binance.com"
+
+export class BinanceCLient{
+    constructor(apiKey, secretKey){
+        this.apiKey = apiKey
+        this.secretKey = secretKey
+
+        this.http = axios.create({
+            baseURL: BASE_URL,
+            headers: {
+                "X-MBX-APIKEY": this.apiKey
+            }
+        })
+    }
+
+    async getPrice(symbol) {
+        const response = await this.http.get("/api/v3/ticker/price", {
+            params: {
+                symbol
+            },
+        })
+        return parseFloat(response.data.price)
+    }
+}
---
+++ testConnection.js
@@ -0,0 +1,10 @@
+import {BinanceClient} from "./binanceClient.js";
+
+async function main(){
+    const apiKey = process.env.BINANCE_API_KEY
+    const secretKey = process.env.BINANCE_SECRET_KEY
+
+    const client = new BinanceClient(apiKey, secretKey)
+    const price = await client.getPrice("BTCUSDT")
+    console.log(`The price of BTCUSDT is ${price}`)
+}
